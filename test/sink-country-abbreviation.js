68f955b29588c928b115c14e1fc0e0b153dfb416
Nick Babcock <nbabcock19@hotmail.com>
2015-02-01 17:42:32 -0500

    Init commit

---
+++ test/sink-country-abbreviation.js
@@ -0,0 +1,32 @@
+var expect = require('chai').expect;
+var sink = require('../lib/countries/sink-country-abbreviation');
+
+describe('sink-country-abbreviation', function() {
+  it('should sink abbreviations', function() {
+    var obj = {
+      countries: {
+        REB: {
+          name: 'Rebel'
+        },
+        FRA: {
+          name: 'France'
+        }
+      }
+    };
+
+    sink(obj);
+    expect(obj.countries).to.have.deep.members([{
+        name: 'Rebel',
+        abbreviation: 'REB'      
+      }, {
+        name: 'France',
+        abbreviation: 'FRA'        
+      }]);
+  });
+
+  it('should make countries an array by default', function() {
+    var obj = {};
+    sink(obj);
+    expect(obj.countries).to.deep.equal([]);
+  });
+});
